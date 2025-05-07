6b645e1b089588764b7c650bf794f0182ffc0936
Igal Tsoiref <itsoiref@redhat.com>
2025-05-07 16:31:58 +0300

    Fix sno creation

--- scripts/dpf.sh
+++ scripts/dpf.sh
@@ -217,7 +217,7 @@ function apply_remaining() {
               "$file" != "$GENERATED_DIR/cert-manager-manifests.yaml" && \
               "$file" != "$GENERATED_DIR/kamaji-manifests.yaml" && \
               "$file" != "$GENERATED_DIR/scc.yaml" ]]; then
-            apply_manifest "$file"
+            retry 5 30 apply_manifest "$file"
             if [[ "$file" =~ .*operator.*\.yaml$ ]]; then
                 log [INFO] "Waiting for operator resources..."
                 sleep 10
