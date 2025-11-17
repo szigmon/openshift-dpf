075a1b63b563fc3bc8240716e5ed5679e7b37287
Eli Elgaev <eelgaev@redhat.com>
2025-11-17 17:07:30 +0200

    Added Trusted SFs to the SF configuration script

--- scripts/gen_template.py
+++ scripts/gen_template.py
@@ -219,6 +219,7 @@ net.ipv4.conf.all.rp_filter=2
 set -ex
 CMD=$1
 PF_TOTAL_SF=$2
+PF_TRUSTED_SF=$3
 
 case $CMD in
     setup) ;;
@@ -228,12 +229,40 @@ case $CMD in
     ;;
 esac
 
+set_GUID_for_SF() {
+    json_output=$(mlnx-sf -a show -j)
+
+    # Iterate over each key in the JSON result
+    echo "$json_output" | jq -c 'to_entries[]' | while read -r entry; do
+        key=$(echo "$entry" | jq -r '.key')
+        sf_netdev=$(echo "$entry" | jq -r '.value.sf_netdev')
+        aux_dev=$(echo "$entry" | jq -r '.value.aux_dev')
+
+        # Read the MAC address from the system file
+        mac_address=$(cat /sys/class/net/"$sf_netdev"/address)
+
+        # Update the MAC address using mlxdevm
+        /opt/mellanox/iproute2/sbin/mlxdevm port function set "$key" hw_addr "$mac_address"
+
+        # Unbind and bind the auxiliary device
+        echo "$aux_dev" > /sys/bus/auxiliary/devices/"$aux_dev"/driver/unbind
+        echo "$aux_dev" > /sys/bus/auxiliary/drivers/mlx5_core.sf/bind
+    done
+}
+
 if [ "$CMD" = "setup" ]; then
     # Create SF on P0 for SFC
     # System SF(index 0) has been removed, so DPF will create SF from index 0
-    for i in $(seq 0 $((PF_TOTAL_SF-1))); do
+    for i in $(seq 0 $((PF_TOTAL_SF - 1 - $PF_TRUSTED_SF))); do
+        # Create SFs with random mac, kernel will allocate random MAC for SF netdev
         /sbin/mlnx-sf --action create --device 0000:03:00.0 --sfnum ${i} || true
     done
+
+    for i in $(seq 101 $((100 + $PF_TRUSTED_SF))); do
+        /sbin/mlnx-sf --action create --device 0000:03:00.0 --sfnum ${i} -t || true
+    done
+    
+    set_GUID_for_SF
 fi
 """)
     ),
@@ -303,7 +332,7 @@ Before=kubelet.service
 
 [Service]
 Type=oneshot
-ExecStart=/bin/bash /usr/local/bin/dpf-configure-sfs.sh setup {{.SFNum}}
+ExecStart=/bin/bash /usr/local/bin/dpf-configure-sfs.sh setup {{.SFNum}} {{.TrustedSFs}}
 
 [Install]
 WantedBy=multi-user.target"""
